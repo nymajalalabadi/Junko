707284c94e029ffe87f7f51bb5e0e05764453d10
nymajalalabadi <nima.jalalabadii@gmail.com>
2024-08-11 18:17:28 +0330

    Added Ticket Controller

---
+++ Junko.Web/Areas/User/Controllers/TicketController.cs
@@ -0,0 +1,29 @@
+﻿using Junko.Application.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Junko.Web.Areas.User.Controllers
+{
+    public class TicketController : UserBaseController
+    {
+        #region constructor
+
+        private readonly IContactService _contactService;
+
+        public TicketController(IContactService contactService)
+        {
+            _contactService = contactService;
+        }
+
+        #endregion
+
+        #region list
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        #endregion
+
+    }
+}
