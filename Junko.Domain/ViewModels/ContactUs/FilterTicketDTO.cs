182308660eb11803cb54dbf8b8fa4c4cc3088dda
nymajalalabadi <nima.jalalabadii@gmail.com>
2024-08-11 22:18:30 +0330

    Added Paging

---
+++ Junko.Domain/ViewModels/ContactUs/FilterTicketDTO.cs
@@ -0,0 +1,43 @@
+﻿using Junko.Domain.Entities.Contacts;
+using Junko.Domain.ViewModels.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Junko.Domain.ViewModels.ContactUs
+{
+    public class FilterTicketDTO : Paging<Ticket>
+    {
+        #region properties
+
+        public string? Title { get; set; }
+
+        public long? UserId { get; set; }
+
+        public FilterTicketState? FilterTicketState { get; set; }
+
+        public TicketSection? TicketSection { get; set; }
+
+        public TicketPriority? TicketPriority { get; set; }
+
+        public FilterTicketOrder? OrderBy { get; set; }
+
+        #endregion
+    }
+
+    public enum FilterTicketState
+    {
+        All,
+        NotDeleted,
+        Deleted
+    }
+
+    public enum FilterTicketOrder
+    {
+        CreateDate_DES,
+        CreateDate_ASC,
+    }
+
+}
