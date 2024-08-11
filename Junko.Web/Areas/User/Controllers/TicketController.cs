a9a17e37269031c316a0d3bc472a25065a7648ab
nymajalalabadi <nima.jalalabadii@gmail.com>
2024-08-11 22:58:45 +0330

    Added AddTicket Razor View

--- Junko.Web/Areas/User/Controllers/TicketController.cs
+++ Junko.Web/Areas/User/Controllers/TicketController.cs
@@ -1,4 +1,6 @@
-﻿using Junko.Application.Services.Interfaces;
+﻿using Junko.Application.Extensions;
+using Junko.Application.Services.Interfaces;
+using Junko.Domain.ViewModels.ContactUs;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Junko.Web.Areas.User.Controllers
@@ -18,12 +20,54 @@ namespace Junko.Web.Areas.User.Controllers
 
         #region list
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index(FilterTicketDTO filter)
+        {
+            filter.UserId = User.GetUserId(); 
+
+            filter.OrderBy = FilterTicketOrder.CreateDate_DES;
+            filter.FilterTicketState = FilterTicketState.NotDeleted;
+
+            var result = await _contactService.FilterTickets(filter);
+
+            return View(result);
+        }
+
+        #endregion
+
+        #region add tiket
+
+        [HttpGet("add-ticket")]
+        public async Task<IActionResult> AddTicket()
         {
             return View();
         }
 
+        [HttpPost("add-ticket"), ValidateAntiForgeryToken]
+        public async Task<IActionResult> AddTicket(AddTicketViewModel ticket)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _contactService.AddUserTicket(ticket, User.GetUserId());
+
+                switch (result)
+                {
+                    case AddTicketResult.Error:
+                        TempData[ErrorMessage] = "عملیات با شکست مواجه شد";
+                        break;
+
+                    case AddTicketResult.Success:
+                        TempData[SuccessMessage] = "تیکت شما با موفقیت ثبت شد";
+                        TempData[InfoMessage] = "پاسخ شما به زودی ارسال خواهد شد";
+                        return RedirectToAction("Index");
+                }
+            }
+
+            return View(ticket);
+        }
+
         #endregion
+
+
 
     }
 }
