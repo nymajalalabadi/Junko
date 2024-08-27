0591f7428e24d7bdba36c9cfc3239d661b33ecc0
nymajalalabadi <nima.jalalabadii@gmail.com>
2024-08-27 18:18:33 +0330

    Added  ProductController

---
+++ Junko.Web/Controllers/ProductController.cs
@@ -0,0 +1,33 @@
+﻿using Junko.Application.Services.Interfaces;
+using Junko.Domain.ViewModels.Products;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Junko.Web.Controllers
+{
+    public class ProductController : SiteBaseController
+    {
+        #region constructor
+
+        private readonly IProductService _productService;
+
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        #endregion
+
+        #region filter products
+
+        [HttpGet("products")]
+        public async Task<IActionResult> FilterProducts(FilterProductDTO filter)
+        {
+            var products = await _productService.FilterProducts(filter);
+
+            return View(products);
+        }
+
+        #endregion
+
+    }
+}
