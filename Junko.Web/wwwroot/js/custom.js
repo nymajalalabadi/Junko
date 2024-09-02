59bc39a2a2ac0af702ef78a6bf56d61b98036dd3
nymajalalabadi <nima.jalalabadii@gmail.com>
2024-09-03 01:30:50 +0330

    Added Product Feature At Product

--- Junko.Web/wwwroot/js/custom.js
+++ Junko.Web/wwwroot/js/custom.js
@@ -213,7 +213,7 @@ $('#add_size_button').on('click', function (e)
         }
         else
         {
-            ShowMessage('اخطار', 'رنگ وارد شده تکراری می باشد', 'warning');
+            ShowMessage('اخطار', 'سایز وارد شده تکراری می باشد', 'warning');
             $('#product_size_name_input').val('').focus();
         }
     }
@@ -246,6 +246,78 @@ function reOrderProductSizeHiddenInputs()
 
         $(hiddenSize).attr('name', 'ProductSizes[' + index + '].Size')
         $(hiddenCount).attr('name', 'ProductSizes[' + index + '].Count');
+    });
+}
+
+$('#add_feature_button').on('click', function (e)
+{
+    e.preventDefault();
+
+    var featureTitle = $('#product_feature_title_input').val();
+    var featureValue = $('#product_feature_value_input').val();
+
+    if (featureTitle !== '' && featureValue !== '') {
+        var currentFeaturesCount = $('#list_of_product_features tr');
+        var currentFeaturesCount = currentFeaturesCount.length;
+
+        var isExistsSelectedFeature = $('[feature-title-hidden-input][value="' + featureTitle + '"]');
+
+        if (isExistsSelectedFeature.length === 0) {
+
+            var featureTitleNode =
+                `<input type="hidden" value="${featureTitle}" name="ProductFeatures[${index}].FeatureTitle" 
+            feature-title-hidden-input="${featureTitle}-${featureValue}">`;
+
+            var featureValueNode =
+                `<input type="hidden" value="${featureValue}" name="ProductFeatures[${index}].FeatureValue" 
+            feature-value-hidden-input="${featureTitle}-${featureValue}">`;
+
+            $('#create_product_form').append(featureTitleNode);
+            $('#create_product_form').append(featureValueNode);
+
+            var featureTableNode =
+                `<tr feature-table-item="$${featureTitle}-${featureValue}"> <td> ${featureTitle} </td> <td> ${featureValue}  </td> <td>
+            <a class="btn btn-danger text-white" onclick="removeProductFeature('${featureTitle}-${featureValue}')">حذف</a> </td>  </tr>`;
+
+            $('#list_of_product_features').append(sizeTableNode);
+
+
+            $('#product_feature_title_input').val('');
+            $('#product_feature_value_input').val('');
+        }
+        else {
+            ShowMessage('اخطار', 'ویژگی وارد شده تکراری می باشد', 'warning');
+            $('#product_feature_value_input').val('').focus();
+        }
+    }
+    else {
+        ShowMessage('اخطار', 'لطفا سایز را به درستی وارد نمایید', 'warning');
+    }
+});
+
+function removeProductFeature(index)
+{
+    $('[product_feature_title_input="' + index + '"]').remove();
+    $('[product_feature_value_input="' + index + '"]').remove();
+    $('[feature-table-item="' + index + '"]').remove();
+
+    reOrderProductFeatureHiddenInputs();
+}
+
+function reOrderProductFeatureHiddenInputs()
+{
+    var hiddenFeatures = $('[feature-title-hidden-input]');
+
+    $.each(hiddenFeatures, function (index, value)
+    {
+        var hiddenFeature = $(value);
+
+        var titleValue = $(value).attr('feature-title-hidden-input');
+
+        var hiddenValue = $('feature-value-hidden-input="' + titleValue + '"]');
+
+        $(hiddenFeature).attr('name', 'ProductFeatures[' + index + '].FeatureTitle')
+        $(hiddenValue).attr('name', 'ProductFeatures[' + index + '].FeatureValue');
     });
 }
 
