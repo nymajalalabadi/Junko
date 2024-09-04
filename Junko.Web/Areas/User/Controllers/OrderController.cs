561b85c0c76282fa1a234c17bf52a17dc88df946
nymajalalabadi <nima.jalalabadii@gmail.com>
2024-09-05 00:31:51 +0330

    Added OrderController

---
+++ Junko.Web/Areas/User/Controllers/OrderController.cs
@@ -0,0 +1,43 @@
+﻿using Junko.Application.Extensions;
+using Junko.Application.Services.Interfaces;
+using Junko.Domain.ViewModels.Orders;
+using Junko.Web.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Junko.Web.Areas.User.Controllers
+{
+    public class OrderController : UserBaseController
+    {
+        #region constructor
+
+        private readonly IOrderService _orderService;
+
+        private readonly IUserService _userService;
+
+        public OrderController(IOrderService orderService, IUserService userService)
+        {
+            _orderService = orderService;
+            _userService = userService;
+        }
+
+        #endregion
+
+        #region add product to open order
+
+        [HttpPost("add-product-to-order")]
+        public async Task<IActionResult> AddProductToOrder(AddProductToOrderDTO order)
+        {
+            if (ModelState.IsValid)
+            {
+                await _orderService.AddProductToOpenOrder(User.GetUserId(), order);
+
+                return JsonResponseStatus.SendStatus(JsonResponseStatusType.Success, "محصول مورد نظر با موفقیت ثبت شد", null);
+            }
+
+            return JsonResponseStatus.SendStatus(JsonResponseStatusType.Danger, "در ثبت اطلاعات خطایی رخ داد", null);
+        }
+
+        #endregion
+
+    }
+}
