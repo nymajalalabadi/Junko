72bf43be749207d464519b7ad9b5e38ab68ea6e3
nymajalalabadi <nima.jalalabadii@gmail.com>
2024-09-05 15:39:47 +0330

    Completed Order Product

--- Junko.Web/wwwroot/js/custom.js
+++ Junko.Web/wwwroot/js/custom.js
@@ -237,9 +237,13 @@ function reOrderProductSizeHiddenInputs() {
 
 ////product color
 
-function changeProductPriceBasedOnColor(priceOfColor, colorName) {
+function changeProductPriceBasedOnColor(colorId, priceOfColor, colorName)
+{
     var basePrice = parseInt($('#ProductBasePrice').val(), 0);
+
     $('.current_price').html((basePrice + priceOfColor) + ' تومان' + ' ( ' + colorName + ' )');
+
+    $('#add_product_to_order_ProductColorId').val(colorId);
 }
 
 ////product color
@@ -250,7 +254,25 @@ function changeProductPriceBasedOnColor(priceOfColor, colorName) {
 
 function onSuccessAddProductToOrder(res)
 {
-
+    if (res.status === 'Success')
+    {
+        ShowMessage('اعلان', res.message);
+    }
+    else
+    {
+        ShowMessage('اعلان', res.message, 'warning');
+    }
 }
+
+function changeProductSize(sizeId) {
+    $('#add_product_to_order_ProductSizeId').val(sizeId);
+}
+
+$('#number_of_products_in_basket').on('change', function (e)
+{
+    var numberOfProducts = parseInt(e.target.value, 0);
+
+    $('#add_product_to_order_Count').val(numberOfProducts);
+});
 
 /////Order
