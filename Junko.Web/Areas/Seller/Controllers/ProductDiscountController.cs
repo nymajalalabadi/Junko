4e6064fff708667bb4a70f1b8916dc5651d22a11
nymajalalabadi <nima.jalalabadii@gmail.com>
2024-09-11 18:02:56 +0330

    Added Create Product Discount

--- Junko.Web/Areas/Seller/Controllers/ProductDiscountController.cs
+++ Junko.Web/Areas/Seller/Controllers/ProductDiscountController.cs
@@ -27,6 +27,12 @@ namespace Junko.Web.Areas.Seller.Controllers
         public async Task<IActionResult> FilterDiscounts(FilterProductDiscountDTO filter)
         {
             var seller = await _sellerService.GetLastActiveSellerByUserId(User.GetUserId());
+
+            if (seller == null)
+            {
+                return NotFound();
+            }
+
             filter.SellerId = seller.Id;
 
             var result = await _discountService.FilterProductDiscount(filter);
@@ -35,6 +41,44 @@ namespace Junko.Web.Areas.Seller.Controllers
         }
 
         #endregion
+
+        #region create discount
+
+        [HttpGet("create-discount")]
+        public IActionResult CreateDiscount()
+        {
+            return View();
+        }
+
+        [HttpPost("create-discount"), ValidateAntiForgeryToken]
+        public async Task<IActionResult> CreateDiscount(CreateProductDiscountDto discount)
+        {
+            if (ModelState.IsValid)
+            {
+                var seller = await _sellerService.GetLastActiveSellerByUserId(User.GetUserId());
+                var result = await _discountService.CreateProductDiscount(discount, seller.Id);
+
+                switch (result)
+                {
+                    case CreateDiscountResult.Error:
+                        TempData[ErrorMessage] = "عملیات ثبت تخفیف مورد نظر با شکست مواجه شد";
+                        break;
+                    case CreateDiscountResult.ProductNotFound:
+                        TempData[WarningMessage] = "محصول مورد نظر یافت نشد";
+                        break;
+                    case CreateDiscountResult.ProductIsNotForSeller:
+                        TempData[WarningMessage] = "محصول مورد نظر یافت نشد";
+                        break;
+                    case CreateDiscountResult.Success:
+                        TempData[SuccessMessage] = "عملیات ثبت تخفیف برای محصول مورد نظر با موفقیت انجام شد";
+                        return RedirectToAction("FilterDiscounts");
+                }
+            }
+            return View(discount);
+        }
+
+        #endregion
+
 
     }
 }
